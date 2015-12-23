e9d2416a6d62089421019345b3587d1d10ab3747
Steven Nichols <steven@steven-nichols.com>
2015-12-22 23:53:02 -0500

    Added sample application

---
+++ src/Samples/ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MayanDate;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a date: ");
+                var input = Console.ReadLine();
+
+                DateTime gregorian;
+                MayanDateTime mayan;
+
+                var success = DateTime.TryParse(input, out gregorian);
+                if (success)
+                {
+                    mayan = new MayanDateTime(gregorian);
+                    Console.WriteLine(mayan);
+                }
+                else
+                {
+                    mayan = MayanDateTime.Parse(input);
+                    Console.WriteLine(mayan);
+                    Console.WriteLine(mayan.ToDateTime());
+                }
+            }
+        }
+    }
+}
